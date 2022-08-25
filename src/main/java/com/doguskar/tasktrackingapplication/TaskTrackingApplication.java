dd6c4728857c51fe8d3fe9e017c7d925197b1a7e
Doğuş Kar <doguskar98@gmail.com>
2022-08-26 01:23:29 +0300

    initial

---
+++ src/main/java/com/doguskar/tasktrackingapplication/TaskTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.doguskar.tasktrackingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doguskar/tasktrackingapplication/TaskTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doguskar.tasktrackingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
